e02580695e8e9443d9c71ba167a718f1790c6099
Brady Vitrano <brady.vitrano@rackspace.com>
2015-07-27 09:55:33 -0500

    feat(jinja): add support for jinja in styleguide

--- styleguide/gulpfile.js
+++ styleguide/gulpfile.js
@@ -4,13 +4,20 @@ var sourcemaps = require('gulp-sourcemaps');
 var less = require('gulp-less');
 var minifyCss = require('gulp-minify-css');
 var watch = require('gulp-watch');
+var nunjucks = require('gulp-nunjucks-html');
+
+var nunjucksOpts = {
+    searchPaths: ['src/templates']
+};
+
+var BUILD_DEST = 'dist/styleguide';
 
 gulp.task('js', function() {
   return gulp.src('src/**/*.js')
     .pipe(sourcemaps.init())
     .pipe(concat('site.js'))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('dist/js'));
+    .pipe(gulp.dest(BUILD_DEST + '/js'));
 });
 
 gulp.task("css", function () {
@@ -20,12 +27,19 @@ gulp.task("css", function () {
     .pipe(less())
     .pipe(minifyCss())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('dist/css'));
+    .pipe(gulp.dest(BUILD_DEST + '/css'));
 });
 
-gulp.task('build', ['js', 'css']);
+gulp.task('nunjucks', function() {
+    return gulp.src('src/pages/**/*.html')
+        .pipe(nunjucks(nunjucksOpts))
+        .pipe(gulp.dest(BUILD_DEST));
+});
+
+gulp.task('build', ['js', 'css', 'nunjucks']);
 
 gulp.task('watch', function(){
   gulp.watch(['src/js/**/*.js'], ['js']);
-  gulp.watch(['src/less/**/*.less'], ['css']);
+    gulp.watch(['src/less/**/*.less'], ['css']);
+    gulp.watch(['src/templates/**/*.html', 'src/pages/**/*.html'], ['nunjucks']);
 });
